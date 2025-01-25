dc34be43ce5969b967d050b66727c0b6163e34f1
sarasocial <>
2025-01-24 22:40:09 -0800

    bbb

---
+++ better-install.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+# Read from dependencies source file
+IFS=$'\n' read -d '' -r -a dependencies < ./resources/installer-dependencies.txt
+
+# Format dependencies into array; keys are package names, values are package types
+# ex: the line 'arch git' indicates package 'git' from official arch repositories
+
+# Isolate missing packages
+missingPackages=()
+for package in "${dependencies[@]}"; do
+    currentPackage=($package)
+    if [[ "${currentPackage[0]}" == "@@" ]]; then
+        packageType="${currentPackage[1]}"
+        packageName="${currentPackage[2]}"
+        # Supported types: 'arch', 'aur', 'makepkg', 'pip'
+        if [[ "$type" == "pip" ]]; then
+            if [[ ! $(pacman -Q "python" &>/dev/null) ]]; then
+                missingPackages+="$currentPackage" # python not installed
+            else
+                python -c "import proton-keyring-linux" && echo $? > result
+                [ ! $result ] && missingPackages+="$currentPackage" # python package not found
+            fi
+        elif [[ ! $(pacman -Q "$packageName" &>/dev/null) ]]; then
+            missingPackages+="$currentPackage" # package not found with pacman
+        fi
+    fi
+done
+
+missingPackages+="@@ EOF"
+
+# Prompt user to install missing dependencies
+if [[ ${#missingPackages[@]} > 0 ]]; then
+
+    # Install dependencies
+    needSudo=1
+    IFS=$'\n' read -d '' -r -a dependencies < ./resources/installer-dependencies
+    while "${missingPackages[@]}"; do
+        if [[ "$1" == "@@" ]]; then
+            shift && packageType="$1" && packageName="$2" && shift 2
+            case $packageType in
+                arch|pacman)
+                    [ $needSudo ] && sudo pacman -S "$packageName" \
+                        || pacman -S "$packages" echo $? > result \
+                        && [ ! $result ] && needSudo=0 \
+                        && sudo pacman -S "$packageName"
+                        shift
+                    ;;
+                aur|paru)
+                    paru -S "$packageName"
+                    ;;
+                makepkg)
+                    source="$1"
+                    args=()
+                    while ! "$1" == "@@"; do 
+                        args+="$1" && shift
+                    done
+                    rm -rf "$packageName"
+                    git clone "$source" "$packageName" && cd "$packageName"
+                    makepkg "$args" && cd .. && rm -rf "$packageName"
+                    ;;
+                pip)
+                    python -m pip install "$packageName" && echo $? > result
+                    [ ! $result ] || sudo pacman -S "$packageName"
+                    shift
+                    ;;
+                EOF)
+                    break
+                    ;;
+                *)
+                    printf "Unknown package type $packageType"
+                    exit
+                    ;;
+            esac
+        else
+            break
+        fi
+    done
+fi
+
+# Run installer
+python ./installer.py
--- testmenu.sh
+++ testmenu.sh
@@ -1,23 +1,125 @@
 #!/bin/bash
 # Bash Menu Script Example
 
-PS3='Please enter your choice: '
-options=("Option 1" "Option 2" "Option 3" "Quit")
-select opt in "${options[@]}"
-do
-    case $opt in
-        "Option 1")
-            echo "you chose choice 1"
-            ;;
-        "Option 2")
-            echo "you chose choice 2"
-            ;;
-        "Option 3")
-            echo "you chose choice $REPLY which is $opt"
-            ;;
-        "Quit")
-            break
-            ;;
-        *) echo "invalid option $REPLY";;
+SelectItemArrows() {	# [--nr] Request_string + Item list (strings). Exit: 0: valid selection, 1: empty list, 90: cancel. Returns (if exit-code: 0) to StdOut:
+local ItemLinesShown=10 # Selected Item/Item's number (if option '--nr' supplied). Notes: Prints Item list + Request string to StdErr so that they are shown in
+local PrintNumber=false # 'Selection=$(SelectItemArrows ...)'. Long lists will be partially shown (10 Lines only) but the user will be able to 'scroll' through
+if [[ "$1" == "--nr" ]] # all Items. Examples: 'SelectItemArrows --nr "Select" One Two 3', 'SelectedItem=$(SelectItemArrows "Select Item" "${ItemList[@]}")'.
+  then
+    PrintNumber=true;shift
+fi
+if [[ $# -lt 2 ]]
+  then		  
+    return 1
+fi
+local Line Key ArrowDown ArrowLeft ArrowRight ArrowUp DeleteLine CursorUp
+while read -r Line
+  do
+    Key="${Line%=*}"
+    case "$Key" in
+      key_down) ArrowDown="${Line#*=}";ArrowDown=$(printf '%b' "${ArrowDown%,*}");;
+      key_left) ArrowLeft="${Line#*=}";ArrowLeft=$(printf '%b' "${ArrowLeft%,*}");;
+      key_right) ArrowRight="${Line#*=}";ArrowRight=$(printf '%b' "${ArrowRight%,*}");;
+      key_up) ArrowUp="${Line#*=}";ArrowUp=$(printf '%b' "${ArrowUp%,*}");;
+      delete_line) DeleteLine="${Line#*=}";DeleteLine="${DeleteLine%,*}";;
+      cursor_up) CursorUp="${Line#*=}";CursorUp="${CursorUp%,*}";;
     esac
-done
+  done < <(infocmp -L1 linux | egrep "key_down|key_left|key_right|key_up|delete_line|cursor_up") # Wrong values in terminal emulator with 'infocmp -L1 $TERM'.
+local Char ItemNr Prefix ArrowPosition=0 ArrowNewPosition=1 ScrollShift=0 ScrollNewShift=0 Request="(↑↓ and →, cancel: ←) $1";shift
+if [[ $# -lt $ItemLinesShown ]]
+  then
+    ItemLinesShown=$#
+fi
+Line=0
+until [[ "$Key" == "$ArrowLeft" || "$Key" == "$ArrowRight" || "$Key" == "$(printf '%b' "\n")" ]] # $ArrowRight or "\n" (Enter): Select; $ArrowLeft: Cancel.
+  do
+    if [[ $ArrowPosition -ne $ArrowNewPosition || $ScrollShift -ne $ScrollNewShift ]]
+      then
+        ArrowPosition=$ArrowNewPosition;ScrollShift=$ScrollNewShift
+        while [[ $Line -gt 0 ]]
+          do # Delete lines written in previous main loop run.
+            ((Line--))
+             printf '%b' "${DeleteLine}${CursorUp}\r${DeleteLine}" 1>&2
+          done # After loop $Line is 0.
+        while [[ $Line -lt $ItemLinesShown ]]
+          do # Write new lines according to new conditions.
+            ((Line++))
+            ((ItemNr = Line + ScrollShift))
+            case  "$Line" in
+              1)
+                if [[ $ScrollShift -eq 0 ]]
+                  then
+                    Prefix=" "
+                  else
+                    Prefix="↑"
+                fi;;
+              ${ItemLinesShown})
+                if [[ $ItemNr -lt $# ]]
+                  then
+                    Prefix="↓"
+                  else
+                    Prefix=" "
+                fi;;
+              *) Prefix=" ";;
+            esac
+            if [[ $ArrowPosition -eq $Line ]]
+              then
+                Prefix="${Prefix}→"
+              else
+                Prefix="${Prefix} "
+            fi
+            printf '%s\n' "${Prefix}'${!ItemNr}'" 1>&2
+          done # After loop $Line is $ItemLinesShown.
+        printf '\n%s' "$Request" 1>&2 # '\n%s': List + Empty line separator + Request. No NewLine after $Request.
+        ((Line++)) # To account for Empty line separator.
+    fi
+    if read -s -r -n 1 Char
+      then
+        Key="$Char"
+        while read -s -n 1 -t 0.01 Char # Timeout (ExitCode != 0) if no more characters available.
+          do
+            Key="${Key}$Char"
+          done
+    fi
+    case $(printf '%b' "$Key") in
+      ${ArrowUp})
+        if [[ $ArrowPosition -gt 1 ]]
+          then
+            ((ArrowNewPosition--))
+          else
+            if [[ $ScrollShift -gt 0 ]]
+              then
+                ((ScrollNewShift--))
+            fi
+        fi;;
+      ${ArrowDown})
+        if [[ $ArrowPosition -lt $ItemLinesShown ]]
+          then
+            ((ArrowNewPosition++))
+          else
+            if [[ $ScrollShift -lt $(($# - ItemLinesShown)) ]]
+              then
+                ((ScrollNewShift++))
+            fi
+        fi;;
+      ${ArrowRight});;
+      ${ArrowLeft});;
+    esac
+  done
+printf '\n' 1>&2 # $Request line was printed without NewLine.
+if [[ "$Key" == "$ArrowLeft" ]]
+  then
+    return 90 # Selection cancelled by user.
+fi # $ArrowRight or "\n" (Enter - undocumented) have been pressed.
+((ItemNr = ArrowPosition + ScrollShift))
+if $PrintNumber
+  then
+    printf '%s\n' $ItemNr
+  else
+    printf '%s\n' "${!ItemNr}"
+fi
+return 0
+}
+
+
+SelectItemArrows "Select a number" One Two Three 4 5 6 7 8 9
